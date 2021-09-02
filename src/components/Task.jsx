31e6e983bacfc09f94baeea07685345a618f2aa5
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-09-02 16:50:50 -0700

    creates task component

---
+++ src/components/Task.jsx
@@ -0,0 +1,9 @@
+const Task = (props) => {
+  return (
+    <div className={`task${props.todo.completed ? " completed" : ""}`}>
+      <p>{props.todo.task}</p>
+    </div>
+  );
+};
+
+export default Task;
