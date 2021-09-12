418c6cf1b7de3e7c1993949ab78f00cc2ceabb49
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-09-12 16:16:22 -0700

    completes ShoppingCard base functionality

--- src/components/shopping/ShoppingCard.jsx
+++ src/components/shopping/ShoppingCard.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ShoppingCard = (props) => {
+  const [listItem, setListItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  const handleChange = (e) => {
+    setListItem(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setItems([...items, { name: listItem, id: Date.now() }]);
+    setListItem("");
+  };
+
   return (
     <div className={"shopping-card-container"}>
       <h3>{props.list.name}</h3>
-      {props.list.tags.map((tag, idx) => (
-        <p key={idx}>{tag}</p>
+      {props.list.tags.map((tag) => (
+        <p key={tag}>{tag}</p>
       ))}
+      <div>
+        {items.map((item) => (
+          <p className={"list-item"} key={item.id}>
+            {item.name}
+          </p>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Add item"
+          value={listItem}
+          onChange={handleChange}
+        />
+        <button type="submit">Add</button>
+      </form>
     </div>
   );
 };
