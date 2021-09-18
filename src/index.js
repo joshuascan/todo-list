de1f470272ed839095f9bd4338971371aea7b505
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-09-18 16:10:01 -0700

    adds redux setup to index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
