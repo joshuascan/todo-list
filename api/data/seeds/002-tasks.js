fc2198bf0696ce88ac239bc9d8eaa7b8fd332bf0
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-10-10 18:53:33 -0700

    fixes seed info

--- api/data/seeds/002-tasks.js
+++ api/data/seeds/002-tasks.js
@@ -5,12 +5,23 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex("tasks").insert([
-        { name: "Clean kitchen", completed: false, moveFromArchive: false },
-        { name: "Update resume", completed: false, moveFromArchive: false },
+        {
+          name: "Clean kitchen",
+          completed: false,
+          moveFromArchive: false,
+          user_id: 1,
+        },
+        {
+          name: "Update resume",
+          completed: false,
+          moveFromArchive: false,
+          user_id: 1,
+        },
         {
           name: "Vacuum living room",
           completed: false,
           moveFromArchive: false,
+          user_id: 1,
         },
       ]);
     });
