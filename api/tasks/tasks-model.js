9542a6f05acf2bdc472ed8be4923db548514ed53
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-10-30 17:37:16 -0700

    creates updateTask model function

--- api/tasks/tasks-model.js
+++ api/tasks/tasks-model.js
@@ -4,6 +4,13 @@ function findAllTasks(user_id) {
   return db("tasks").where("user_id", user_id);
 }
 
+function findById(user_id, task_id) {
+  return db("tasks")
+    .where("user_id", user_id)
+    .andWhere("task_id", task_id)
+    .first();
+}
+
 async function addTask(user_id, task) {
   const [newTask] = await db("tasks").insert(
     {
@@ -13,6 +20,19 @@ async function addTask(user_id, task) {
     ["user_id", "task_id", "name", "completed", "moveFromArchive"]
   );
   return newTask;
+}
+
+async function updateTask(user_id, task_id, task) {
+  const [updatedTask] = await db("tasks")
+    .update({ user_id: user_id, name: task.name }, [
+      "user_id",
+      "task_id",
+      "name",
+      "completed",
+      "moveFromArchive",
+    ])
+    .where("task_id", task_id);
+  return updatedTask;
 }
 
 function findAllArchivedTasks(user_id) {
@@ -32,7 +52,9 @@ async function addToArchive(user_id, task) {
 
 module.exports = {
   findAllTasks,
+  findById,
   addTask,
+  updateTask,
   findAllArchivedTasks,
   addToArchive,
 };
