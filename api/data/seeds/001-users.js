2da6e3895aba324668c1f6b8d1fbc5aa0ca37318
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-11-08 12:57:23 -0800

    adds bcrypt hash to users seed

--- api/data/seeds/001-users.js
+++ api/data/seeds/001-users.js
@@ -1,9 +1,13 @@
+const bcrypt = require("bcryptjs");
+
+const hash = bcrypt.hashSync("password", 8);
+
 exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex("users")
     .del()
     .then(function () {
       // Inserts seed entries
-      return knex("users").insert([{ username: "josh", password: "password" }]);
+      return knex("users").insert([{ username: "josh", password: hash }]);
     });
 };
