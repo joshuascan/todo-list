d4ff6ed2d94415c65f093ebf2c5b6200c03e2912
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-11-13 18:53:39 -0800

    adds logout functionality

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { logout } from "../store/userSlice";
 
 const Navigation = () => {
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   return (
     <nav>
       <Link to="/tasks">Tasks</Link>
       <Link to="/shopping">Lists</Link>
       <Link to="/notes">Notes</Link>
+      <Link to="/login" onClick={() => handleLogout()}>
+        Logout
+      </Link>
     </nav>
   );
 };
