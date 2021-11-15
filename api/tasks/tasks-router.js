09b27b5ddaec952fe4c83857f71365dcfcb5bd86
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-11-15 12:46:33 -0800

    changes order of task endpoints

--- api/tasks/tasks-router.js
+++ api/tasks/tasks-router.js
@@ -1,6 +1,37 @@
 const router = require("express").Router();
 const Tasks = require("./tasks-model");
 
+// ARCHIVE ENDPOINTS
+
+router.get("/archived", (req, res, next) => {
+  Tasks.findAllArchivedTasks(req.decodedToken.subject)
+    .then((tasks) => {
+      res.status(200).json(tasks);
+    })
+    .catch(next);
+});
+
+router.post("/archived", (req, res, next) => {
+  const addedTasks = req.body;
+  const newArchivedTasks = addedTasks.map((task) => ({
+    ...task,
+    user_id: req.decodedToken.subject,
+  }));
+  Tasks.addToArchive(newArchivedTasks)
+    .then((task) => {
+      res.status(201).json(task);
+    })
+    .catch(next);
+});
+
+router.delete("/archived", (req, res, next) => {
+  Tasks.clearArchive(req.decodedToken.subject)
+    .then((tasks) => {
+      res.status(200).json(tasks);
+    })
+    .catch(next);
+});
+
 // TASK ENDPOINTS
 
 router.get("/", (req, res, next) => {
@@ -11,13 +42,13 @@ router.get("/", (req, res, next) => {
     .catch(next);
 });
 
-// router.get("/:task_id", (req, res, next) => {
-//   Tasks.findById(req.decodedToken.subject, req.params.task_id)
-//     .then((task) => {
-//       res.status(200).json(task);
-//     })
-//     .catch(next);
-// });
+router.get("/:task_id", (req, res, next) => {
+  Tasks.findById(req.decodedToken.subject, req.params.task_id)
+    .then((task) => {
+      res.status(200).json(task);
+    })
+    .catch(next);
+});
 
 router.post("/", (req, res, next) => {
   Tasks.addTask(req.decodedToken.subject, req.body)
@@ -45,38 +76,6 @@ router.delete("/:task_id", (req, res, next) => {
 
 router.delete("/", (req, res, next) => {
   Tasks.clearCompleted(req.decodedToken.subject)
-    .then((tasks) => {
-      res.status(200).json(tasks);
-    })
-    .catch(next);
-});
-
-// ARCHIVE ENDPOINTS
-
-router.get("/archived", (req, res, next) => {
-  Tasks.findAllArchivedTasks(req.decodedToken.subject)
-    .then((tasks) => {
-      res.status(200).json(tasks);
-    })
-    .catch(next);
-});
-
-router.post("/archived", (req, res, next) => {
-  const addedTasks = req.body;
-  const newArchivedTasks = addedTasks.map((task) => ({
-    ...task,
-    user_id: req.decodedToken.subject,
-  }));
-  console.log(newArchivedTasks);
-  Tasks.addToArchive(newArchivedTasks)
-    .then((task) => {
-      res.status(201).json(task);
-    })
-    .catch(next);
-});
-
-router.delete("/archived", (req, res, next) => {
-  Tasks.clearArchive(req.decodedToken.subject)
     .then((tasks) => {
       res.status(200).json(tasks);
     })
