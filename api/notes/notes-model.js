be98f48a04bb12430049dcb69d8b7ab5272740bc
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-11-16 15:39:22 -0800

    creates CRUD model functions for notes

--- api/notes/notes-model.js
+++ api/notes/notes-model.js
@@ -1,3 +1,41 @@
 const db = require("../data/db-config");
 
-module.exports = {};
+function findAllNotes(user_id) {
+  return db("tasks").where("user_id", user_id);
+}
+
+async function addNote(note) {
+  const [newNote] = await db("tasks").insert(note, [
+    "note_id",
+    "title",
+    "body",
+  ]);
+  return newNote;
+}
+
+async function updateNote(user_id, note_id, note) {
+  const [updatedNote] = await db("notes")
+    .update(
+      {
+        ...note,
+        user_id: user_id,
+      },
+      ["note_id", "title", "body"]
+    )
+    .where("note_id", note_id);
+  return updatedNote;
+}
+
+async function removeNote(note_id) {
+  const [removedNote] = await db("notes")
+    .del(["note_id", "title", "body"])
+    .where("note_id", note_id);
+  return removedNote;
+}
+
+module.exports = {
+  findAllNotes,
+  addNote,
+  updateNote,
+  removeNote,
+};
